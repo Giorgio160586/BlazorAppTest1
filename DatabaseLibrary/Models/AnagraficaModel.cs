0904e07722931c12031919b2cddf6d6b955d4b5e
Giorgio <giorgio160586@gmail.com>
2020-08-24 17:43:06 +0200

    Add project files.

---
+++ DatabaseLibrary/Models/AnagraficaModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DatabaseLibrary.Models
+{
+    public class AnagraficaModel
+    {
+        public Int32 ID { get; set; }
+        public Double Nome { get; set; }
+    }
+}
