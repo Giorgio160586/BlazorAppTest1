1f65d8adc27f7483e8980d48f2a178b8ecdfd99d
Giorgio <giorgio160586@gmail.com>
2020-08-26 12:32:24 +0200

    aggiunto InserisciAnagrafica

--- DatabaseLibrary/Anagrafica.cs
+++ DatabaseLibrary/Anagrafica.cs
@@ -14,19 +14,21 @@ namespace DatabaseLibrary
         {
             configurationManager = config;
         }
-        public async Task<List<AnagraficaModel>> Select<AnagraficaModel>(object param = null)
+        public async Task<List<AnagraficaModel>> Select<AnagraficaModel>()
         {
             using (var conn = new SqlConnection(configurationManager.GetConnectionString("conn")))
             {
-                var ret = await conn.QueryAsync<AnagraficaModel>("SELECT * FROM Anagrafica", param);
+                var ret = await conn.QueryAsync<AnagraficaModel>("SELECT * FROM Anagrafica");
                 return ret.ToList();
             }
         }
-        public async Task Insert<AnagraficaModel>(object param = null)
+        public async Task<AnagraficaModel> Insert<AnagraficaModel>(AnagraficaModel param)
         {
             using (var conn = new SqlConnection(configurationManager.GetConnectionString("conn")))
             {
-              await conn.ExecuteAsync("INSERT INTO Anagrafica (Nome) VALUES (@Nome)", param);
+                var ret = await conn.QueryAsync<AnagraficaModel>(@"INSERT INTO Anagrafica (Nome) VALUES (@Nome);
+                                                  SELECT * FROM Anagrafica WHERE ID = CAST(SCOPE_IDENTITY() as int)", param);
+                return ret.FirstOrDefault();
             }
         }
     }
